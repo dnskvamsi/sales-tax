33f79da2f2bd5367d73b46b09139e833c229e1a6
NSKVamsi Devarapalli <naga.devarapalli.18@mountblue.tech>
2022-01-28 15:04:39 +0530

    added file creator class

--- sales_tax_calculator.rb
+++ sales_tax_calculator.rb
@@ -3,7 +3,7 @@ require_relative "Tax"
 require 'net/http'
 require 'json'
 
-def fetch_conversion_rates()
+def fetch_conversion_rates(convert_to)
     url = "http://data.fixer.io/api/latest?access_key=93cae40df10b0521f99e1271a38794b9&base=EUR&symbols=INR,USD,EUR"
     uri = URI(url)
     response = Net::HTTP.get(uri)
@@ -11,41 +11,8 @@ def fetch_conversion_rates()
     if(convert["success"]==false)
         convert["rates"]={"USD"=>1.12,"EUR"=>1,"INR"=>83.58}
     end
-    return convert
+    return convert["rates"][convert_to]
 end
-
-
-def get_items_from_the_user()
-    items=[]
-    loop do   
-        print("Enter Quantity in Integers: ")
-        begin
-            qty=Integer(gets().chomp().strip())
-        rescue => exception
-            puts("**Please Enter a valid quantity of integer type**")
-            qty=gets().chomp().strip().to_i
-        end
-        print("Enter Item Description: ")
-        item_description = gets().chomp().strip()
-        print("Enter Shelf Price of the Item: ")
-        begin
-            price= Float(gets().chomp().strip())
-        rescue => exception
-            puts("**Please enter a valid price: **")
-            price= gets().chomp().strip().to_f
-        end
-        add_item(Item.new(qty,item_description,price),items)
-        print("To Exit press q or press any other key to add more items: ")
-        add_or_quit=gets().chomp().strip().downcase()
-        break if add_or_quit =="q"
-    end
-    return items
-end
-
-def add_item(item,item_arr)
-    item_arr.push(item)
-end
-
 
 def total_calculator(items)
     total_price=0
@@ -58,39 +25,79 @@ def total_calculator(items)
 end
 
 
-def write_to_file(items)
+def get_items_from_the_user()
+    items=[]
+    loop do   
+        print("Enter Quantity in Integers: ")
+        qty=gets().chomp().strip().to_i
+        print("Enter Item Description: ")
+        item_description = gets().chomp().strip()
+        print("Enter Shelf Price of the Item: ")
+        price= gets().chomp().strip().to_f
+        add_item(Item.new(qty,item_description,price),items)
+        print("To Exit press q: ")
+        add_or_quit=gets().chomp().strip().downcase()
+        break if add_or_quit =="q"
+    end
+    return items
+end
+
+def add_item(item,item_arr)
+    item_arr.push(item)
+end
+
+
+class FileCreator
+    attr_accessor :data,:extension,:delimiter,:file_name
+    def initialize(data,file_name,extension,delimiter)
+        @data = data
+        @extension = extension
+        @delimiter = delimiter
+        @file_name = file_name
+    end
+    def write()
+        fileobject=File.open("#{@file_name}"+"#{@extension}","a")
+        for row in @data
+             fileobject.write("#{row.join(@delimiter)}\n")
+        end
+    end
+end
+
+def data_generator()
+    items=get_items_from_the_user()
     print("please select the conversion INR,USD,EUR: ")
     convert_to = gets().chomp().strip()
-    convert=fetch_conversion_rates()
+    convert=fetch_conversion_rates(convert_to)
     total_tax=0
     total_price=0
     puts("Enter the extension to save the file: ")
     puts("1. Text\n2. CSV")
     extension=gets().chomp().strip()
+    print("Enter the name of the file without extension: ")
+    file_name=gets().chomp().strip()
     delimiter={"1"=>"|","2"=>","}
     if(extension=="1")
         file_extension=".txt"
     elsif(extension=="2") 
-    file_extension =".csv"
+        file_extension =".csv"
     end
-   fileobject= File.open("tax"+"#{file_extension}","a")
-   fileobject.write("Qty\
-        #{delimiter[extension]} Item_description\
-        #{delimiter[extension]} Price\
-        #{delimiter[extension]} Item_tax\n")
-    total_price,total_tax= total_calculator(items)
+    total_price,total_tax=total_calculator(items)
+    data=[]
+    data.push(["Qty","Item_description","Price","Item_tax"])
     for item in items
-        if convert_to == "INR" || convert_to == "USD" || convert_to=="EUR"
-            fileobject.write("#{item.qty} \
-    #{delimiter[extension]} #{item.item_description} \
-    #{delimiter[extension]} #{(item.price*convert["rates"][convert_to]).round(2)} \
-    #{delimiter[extension]} #{(item.item_tax*convert["rates"][convert_to]).round(2)}\n")
-        end
+        item_data=[]
+        item_data.push(item.qty,item.item_description)
+        item_data.push((item.price*convert).round(2))
+        item_data.push((item.item_tax*convert).round(2))
+        data.push(item_data)
     end
-    fileobject.write("Total-tax: #{(total_tax*convert["rates"][convert_to]).round(2)}\nTotal_price: #{(total_price*convert["rates"][convert_to]).round(2)}\n")
-    
+    data.push(["total_price: #{(total_price*convert).round(2)}"],["total_tax: #{(total_tax*convert).round(2)}"])
+    file=FileCreator.new(data,file_name,file_extension,delimiter[extension])
+    file.write()
 end
 
+
+
 if $PROGRAM_NAME == __FILE__
-    write_to_file(get_items_from_the_user())
+    data_generator()
 end
